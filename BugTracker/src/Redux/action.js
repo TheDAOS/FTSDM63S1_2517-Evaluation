db45ebd0eaa737913fd7806e603edc2ffc3cb2e2
theDAOS <sanjusanthosh46@gmail.com>
2025-04-28 13:28:51 +0530

    Json Upload done

--- BugTracker/src/Redux/action.js
+++ BugTracker/src/Redux/action.js
@@ -1,6 +1,6 @@
 import { UPLOAD_LOG } from "./actionType";
 
-export const upload_log = (log) => ({
+export const uploadLog = (log) => ({
     type: UPLOAD_LOG,
-    payload: log.json(),
+    payload: log,
 });
--- BugTracker/src/components/Upload.jsx
+++ BugTracker/src/components/Upload.jsx
@@ -1,21 +1,73 @@
-import { Box, Button, Card, Flex, Textarea } from "@chakra-ui/react"
+import { Box, Button, Card, Flex, Textarea, Field } from "@chakra-ui/react"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { uploadLog } from "../Redux/action";
+import { useNavigate } from "react-router";
+
 
 function Upload() {
+    const [textArea, setTextArea] = useState('');
+    const [error, setError] = useState({ error: false, message: '' })
+    const dispatch = useDispatch();
+    let navigate = useNavigate();
+
+    function handleUpload() {
+        try {
+            let data = JSON.parse(textArea);
+            // { "id": 1, "timestamp": 1710000034, "severity": "high", "message": "Unhandled promise", "devId": "DEV1001" }
+            if (
+                data.id &&
+                data.timestamp &&
+                data.severity &&
+                data.message &&
+                data.devId
+            ) {
+                console.log('jj');
+                setError({ error: false, message: '' })
+                dispatch(uploadLog(data))
+                navigate("/logs");
+
+            } else {
+                setError({
+                    error: true,
+                    message: 'Data not complete!'
+                })
+                console.log('error');
+            }
+        } catch (error) {
+            setError({
+                error: true,
+                message: 'Input Invalid!'
+            })
+            console.log("handleUpload error", error);
+        }
+    }
+
     return (
         <Card.Root w='100%' background='bg.muted'>
-            <Card.Header textAlign='center' fontWeight='bold' fontSize='xl'> Upload json Log Here</Card.Header>
+            <Card.Header textAlign='center' fontWeight='bold' fontSize='xl'> Upload JSON Log Here</Card.Header>
             <Card.Body>
-                <Textarea
-                    size='lg'
-                    background='bg'
-                    placeholder="Add JSON text here"
-                    autoresize
-                />
+
+                <Field.Root invalid={error.error}>
+                    <Field.Label>
+                        JSON <Field.RequiredIndicator />
+                    </Field.Label>
+                    <Textarea
+                        size='lg'
+                        background='bg'
+                        placeholder="Add JSON text here"
+                        autoresize
+                        value={textArea}
+                        onChange={(e) => setTextArea(e.target.value)}
+                    />
+                    <Field.ErrorText>{error.message}</Field.ErrorText>
+                </Field.Root>
 
                 <Flex mt={3} me={1} justify='end' align='center'>
                     <Button
                         variant="outline"
                         colorPalette="teal"
+                        onClick={handleUpload}
                     >Upload</Button>
                 </Flex>
             </Card.Body>
