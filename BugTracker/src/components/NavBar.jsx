ec71ccf19926f6ae2dc6f8a4a0f355391e75c94e
theDAOS <sanjusanthosh46@gmail.com>
2025-04-28 12:08:19 +0530

    NavBar

--- BugTracker/src/components/NavBar.jsx
+++ BugTracker/src/components/NavBar.jsx
@@ -1,12 +1,36 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link } from "@chakra-ui/react";
+import { NavLink } from "react-router";
 
 function NavBar() {
     return (
         <Box background='bg.muted'>
             <Flex justify='space-between' align='center' p={3}>
                 <Heading>BugTracker Pro</Heading>
-            </Flex>
-        </Box>
+
+                <Flex justify='space-between' align='center' gap={5}>
+                    <Link
+                        variant="underline"
+                        colorPalette="teal"
+                    >
+                        <NavLink to="/">Home</NavLink>
+                    </Link>
+                    <Link
+                        variant="underline"
+                        colorPalette="teal"
+                    >
+                        <NavLink to="/upload">Upload</NavLink>
+                    </Link>
+                    <Link
+                        variant="underline"
+                        colorPalette="teal"
+                    >
+                        <NavLink to="/logs">Logs</NavLink>
+                    </Link>
+                </Flex>
+
+                <Heading color='bg.muted'>BugTracker Pro</Heading>
+            </Flex >
+        </Box >
     )
 }
 
