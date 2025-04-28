81215621b1fdd67ff4a7b17c2a82a6046c56b115
theDAOS <sanjusanthosh46@gmail.com>
2025-04-28 11:58:52 +0530

    Router

--- BugTracker/src/App.jsx
+++ BugTracker/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
-    </>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/upload" element={<h1>upload</h1>} />
+        <Route path="/logs" element={<h1>Logs</h1>} />
+        <Route path="/search/:devId" element={<h1>/search/:devId</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App
