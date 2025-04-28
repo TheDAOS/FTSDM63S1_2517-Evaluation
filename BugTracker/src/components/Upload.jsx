aa22bfb84c52aeb663500680bb7b6e13119b51e4
theDAOS <sanjusanthosh46@gmail.com>
2025-04-28 13:07:24 +0530

    Upload json

--- BugTracker/src/components/Upload.jsx
+++ BugTracker/src/components/Upload.jsx
@@ -1,6 +1,25 @@
+import { Box, Button, Card, Flex, Textarea } from "@chakra-ui/react"
+
 function Upload() {
     return (
-        <h1>Upload App</h1>
+        <Card.Root w='100%' background='bg.muted'>
+            <Card.Header textAlign='center' fontWeight='bold' fontSize='xl'> Upload json Log Here</Card.Header>
+            <Card.Body>
+                <Textarea
+                    size='lg'
+                    background='bg'
+                    placeholder="Add JSON text here"
+                    autoresize
+                />
+
+                <Flex mt={3} me={1} justify='end' align='center'>
+                    <Button
+                        variant="outline"
+                        colorPalette="teal"
+                    >Upload</Button>
+                </Flex>
+            </Card.Body>
+        </Card.Root>
     )
 }
 
